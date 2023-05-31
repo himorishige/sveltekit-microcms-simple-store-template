d84c0443917471faa7830638bdcce9440609b364
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2023-05-31 11:12:11 +0900

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Footer from '$lib/components/footer/Footer.svelte';
+	import '../app.postcss';
+	import Header from '$lib/components/header/Header.svelte';
+
+	export let data;
+</script>
+
+<div class="app">
+	<Header shopName={data.config?.shopName} />
+
+	<main class="container mx-auto">
+		<slot />
+	</main>
+
+	<Footer serverTime={data.serverDate} />
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+	import Gauge from '$lib/components/icons/Gauge.svelte';
+	import Calculator from '$lib/components/icons/Calculator.svelte';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<div class="min-h-[calc(100dvh-52px-72px)]">
+	<div class="grid gap-10 p-10 md:grid-cols-2">
+		<div>
+			<a
+				href="/dashboard"
+				class="flex flex-col items-center justify-center rounded-lg border border-gray-200 bg-white py-10 shadow hover:bg-gray-100"
+			>
+				<div>
+					<Gauge width={80} height={80} />
+				</div>
+				<h5 class="text-2xl font-bold tracking-tight text-gray-800">Dashboard</h5>
+			</a>
+		</div>
+		<div>
+			<a
+				href="/checkout"
+				class="flex flex-col items-center justify-center rounded-lg border border-gray-200 bg-white py-10 shadow hover:bg-gray-100"
+			>
+				<div>
+					<Calculator width={80} height={80} />
+				</div>
+				<h5 class="text-2xl font-bold tracking-tight text-gray-800">Checkout</h5>
+			</a>
+		</div>
+	</div>
+</div>
